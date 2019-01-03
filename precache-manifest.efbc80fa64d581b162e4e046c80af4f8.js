c790a9b05bd8f6377a49e4d90a809952213fb1d3
Shreshth <shreshth28srivastava@gmail.com>
2019-01-03 12:27:38 +0530

    Updates

---
+++ precache-manifest.efbc80fa64d581b162e4e046c80af4f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a95c78c8b1e07287e07",
+    "url": "/roboFriends/static/js/runtime~main.1a95c78c.js"
+  },
+  {
+    "revision": "0f074f0b75527c16d5e7",
+    "url": "/roboFriends/static/js/main.0f074f0b.chunk.js"
+  },
+  {
+    "revision": "a66c933d16c2c72f404c",
+    "url": "/roboFriends/static/js/1.a66c933d.chunk.js"
+  },
+  {
+    "revision": "0f074f0b75527c16d5e7",
+    "url": "/roboFriends/static/css/main.3ff5d183.chunk.css"
+  },
+  {
+    "revision": "a66c933d16c2c72f404c",
+    "url": "/roboFriends/static/css/1.18d93b02.chunk.css"
+  },
+  {
+    "revision": "5a3f2ad3488aa35a7b0a8ce668e49b9e",
+    "url": "/roboFriends/index.html"
+  }
+];
